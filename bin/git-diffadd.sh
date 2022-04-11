f3eaae3ddc30c0eee174b837a18c9824178f7700
Konstantin Ivanov <KEIvanov@gmail.com>
2022-04-11 21:42:31 +0300

    git tool for review and stage files for commit

---
+++ bin/git-diffadd.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+#
+# First check if some files was added to commit, but then updated
+# this looks like "MM" or "AM"
+#
+# If no files was added look for mofified files "M"
+# NOTE: the same picture will be appeared when all files added to commit
+#       so, use git status first and don't run this tool if no files available for diff
+#
+
+modified=`git status --porcelain | grep '^\s*\SM\s' | awk '{ print $2 }'`
+
+[[ "$modified" == "" ]] && modified=`git status --porcelain | grep '^\s*M\s' | awk '{ print $2 }'`
+
+for file in $modified; do
+  git difftool -y $file
+  read -r -p "$file: Add to commit? [Y/n] " ans
+
+  [[ "$ans" != [nN] ]] && git add $file
+done
